e331b4f840a7ec117c29d921537fc7acadc8cb88
heyethereum <heyethereum@gmail.com>
2022-10-20 19:50:12 +0800

    working backend after upgrading to java19

--- backend/src/main/java/com/backend/BackendApplication.java
+++ backend/src/main/java/com/backend/BackendApplication.java
@@ -1,13 +1,33 @@
 package com.backend;
 
-import org.springframework.boot.SpringApplication;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.StandardEnvironment;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		Map<String, Object> env = Dotenv.load()
+				.entries()
+				.stream()
+				.collect(Collectors.toMap(DotenvEntry::getKey, DotenvEntry::getValue));
+		new SpringApplicationBuilder(BackendApplication.class)
+				.environment(new StandardEnvironment() {
+					@Override
+					protected void customizePropertySources(MutablePropertySources propertySources) {
+						super.customizePropertySources(propertySources);
+						propertySources.addLast(new MapPropertySource("dotenvProperties", env));
+					}
+				}).run(args);
 	}
 
 }
