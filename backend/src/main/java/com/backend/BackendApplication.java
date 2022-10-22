1c3c68e2af7f81a21ff4890648b612d9a566a27c
heyethereum <heyethereum@gmail.com>
2022-10-22 20:09:43 +0800

    edit main.java

--- backend/src/main/java/com/backend/BackendApplication.java
+++ backend/src/main/java/com/backend/BackendApplication.java
@@ -1,33 +1,13 @@
 package com.backend;
 
-import java.util.Map;
-import java.util.stream.Collectors;
-
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.core.env.MapPropertySource;
-import org.springframework.core.env.MutablePropertySources;
-import org.springframework.core.env.StandardEnvironment;
-
-import io.github.cdimascio.dotenv.Dotenv;
-import io.github.cdimascio.dotenv.DotenvEntry;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		Map<String, Object> env = Dotenv.load()
-				.entries()
-				.stream()
-				.collect(Collectors.toMap(DotenvEntry::getKey, DotenvEntry::getValue));
-		new SpringApplicationBuilder(BackendApplication.class)
-				.environment(new StandardEnvironment() {
-					@Override
-					protected void customizePropertySources(MutablePropertySources propertySources) {
-						super.customizePropertySources(propertySources);
-						propertySources.addLast(new MapPropertySource("dotenvProperties", env));
-					}
-				}).run(args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
