59f52a11add5241455fc2bfcf654eb4050eebaad
heyethereum <heyethereum@gmail.com>
2022-10-22 19:52:08 +0800

    started on view clinic public route

--- frontend/src/components/Clinics.js
+++ frontend/src/components/Clinics.js
@@ -1,29 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
-const Service = () => {
+const Clinics = () => {
+  useEffect(() => {
+    const getClinicsFromDB = () => {};
+    getClinicsFromDB();
+  }, []);
+
   return (
     <>
       <section className="service-wrapper py-3">
         <div className="container-fluid pb-3">
           <div className="row">
             <h2 className="h2 text-center col-12 py-5 semi-bold-600">
-              Services
+              Clinics
             </h2>
             <div className="service-header col-2 col-lg-3 text-end light-300">
               <i className="bx bx-gift h3 mt-1" />
             </div>
             <div className="service-heading col-10 col-lg-9 text-start float-end light-300">
               <h2 className="h3 pb-4 typo-space-line">
-                Make Success for future
+                Vacinations or COVID-19 Test
               </h2>
             </div>
           </div>
           <p className="service-footer col-10 offset-2 col-lg-9 offset-lg-3 text-start pb-3 text-muted px-2">
-            You are free to use this template for your commercial or business
-            websites. You are not allowed to re-distribute this template ZIP
-            file on any template collection websites. It is too easy to
-            illegally copy and repost this template.
+            All clinics available around the island for booking Vacinations or
+            COVID-19 Test
           </p>
         </div>
         <div className="service-tag py-5 bg-secondary">
@@ -44,7 +47,7 @@ const Service = () => {
                   href="#"
                   data-filter=".graphic"
                 >
-                  Graphics
+                  North
                 </Link>
               </li>
               <li className="filter-btn nav-item mx-lg-4">
@@ -53,7 +56,7 @@ const Service = () => {
                   href="#"
                   data-filter=".ui"
                 >
-                  UI/UX
+                  South
                 </Link>
               </li>
               <li className="nav-item mx-lg-4">
@@ -62,7 +65,25 @@ const Service = () => {
                   href="#"
                   data-filter=".branding"
                 >
-                  Branding
+                  East
+                </Link>
+              </li>
+              <li className="nav-item mx-lg-4">
+                <Link
+                  className="filter-btn nav-link btn-outline-primary rounded-pill text-light px-4 light-300"
+                  href="#"
+                  data-filter=".branding"
+                >
+                  West
+                </Link>
+              </li>
+              <li className="nav-item mx-lg-4">
+                <Link
+                  className="filter-btn nav-link btn-outline-primary rounded-pill text-light px-4 light-300"
+                  href="#"
+                  data-filter=".branding"
+                >
+                  Central
                 </Link>
               </li>
             </ul>
@@ -85,11 +106,12 @@ const Service = () => {
               <div className="service-work-vertical card-img-overlay d-flex align-items-end">
                 <div className="service-work-content text-left text-light">
                   <span className="btn btn-outline-light rounded-pill mb-lg-3 px-lg-4 light-300">
-                    UI/UX design
+                    Lim's and Partners Toa Payoh Clinic
                   </span>
                   <p className="card-text">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing
+                    190 Toa Payoh Lorong 2 #01-1190 Singapore 310190
                   </p>
+                  <p className="card-text">Contact: 67778890</p>
                 </div>
               </div>
             </Link>
@@ -264,9 +286,50 @@ const Service = () => {
           </div>
           {/* End Recent Work */}
         </div>
+        {/* Start of Pagination */}
+        <div className="row">
+          <div
+            className="btn-toolbar justify-content-center pb-4"
+            role="toolbar"
+            aria-label="Toolbar with button groups"
+          >
+            <div
+              className="btn-group me-2"
+              role="group"
+              aria-label="First group"
+            >
+              <button type="button" className="btn btn-secondary text-white">
+                Previous
+              </button>
+            </div>
+            <div
+              className="btn-group me-2"
+              role="group"
+              aria-label="Second group"
+            >
+              <button type="button" className="btn btn-light">
+                1
+              </button>
+            </div>
+            <div
+              className="btn-group me-2"
+              role="group"
+              aria-label="Second group"
+            >
+              <button type="button" className="btn btn-secondary text-white">
+                2
+              </button>
+            </div>
+            <div className="btn-group" role="group" aria-label="Third group">
+              <button type="button" className="btn btn-secondary text-white">
+                Next
+              </button>
+            </div>
+          </div>
+        </div>
       </section>
     </>
   );
 };
 
-export default Service;
+export default Clinics;
