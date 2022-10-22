9c0d51880ae1e20067385883257827bf9db9140a
heyethereum <heyethereum@gmail.com>
2022-10-22 15:50:23 +0800

    Routing redefined

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,22 +10,33 @@ import "./App.css";
 import Login from "./components/Login";
 import UserDetails from "./pages/UserDetails";
 import Unauthorized from "./components/Unauthorized";
+import Layout from "./components/Layout";
+import Missing from "./components/Missing";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/work" element={<Work />}></Route>
-          <Route path="/worksingle" element={<WorkSingle />}></Route>
-          <Route path="/pricing" element={<Pricing />}></Route>
-          <Route path="/contact" element={<Contact />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/userDetails" element={<UserDetails />}></Route>
-          <Route path="/unauthorized" element={<Unauthorized />}></Route>
+          <Route path="/" element={<Layout />}>
+            {/* public routes */}
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/work" element={<Work />}></Route>
+            <Route path="/worksingle" element={<WorkSingle />}></Route>
+            <Route path="/pricing" element={<Pricing />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/register" element={<Register />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/userDetails" element={<UserDetails />}></Route>
+            <Route path="/unauthorized" element={<Unauthorized />}></Route>
+
+            {/* protected routes */}
+
+            {/* catch all */}
+            <Route path="*" element={<Missing />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
