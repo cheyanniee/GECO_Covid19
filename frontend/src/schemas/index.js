5778da5b4366e3aa2c3d4178efcb441540c5b7b3
cheyanniee <60493204+cheyanniee@users.noreply.github.com>
2022-10-25 00:31:28 +0800

    Register Doctor Integrated with Backend

--- frontend/src/schemas/index.js
+++ frontend/src/schemas/index.js
@@ -28,6 +28,20 @@ export const INITIAL_ADMIN_SEARCH_VALUES = {
   id: "",
 }
 
+export const INITIAL_DOCTOR_FORM_VALUES = {
+  firstName: "",
+  lastName: "",
+  officialId: "",
+  email: "",
+  password: "",
+  confirm_password: "",
+  dob: "",
+  phone: "",
+  address: "",
+  postcode: "",
+  placeId: "",
+};
+
 export const registerSchema = yup.object().shape({
   firstName: yup.string().required("Required"),
   lastName: yup.string().required("Required"),
@@ -107,4 +121,44 @@ export const userDetailsSchema = yup.object().shape({
     .integer()
     .min(100000, "Postal code must be exactly 6 digits")
     .max(1000000, "Postal code must be at exactly 6 digits"),
+});
+
+export const registerDoctorSchema = yup.object().shape({
+  firstName: yup.string().required("Required"),
+  lastName: yup.string().required("Required"),
+  officialId: yup.string().required("Required"),
+  email: yup.string().email("Please enter a valid email").required("Required"),
+  password: yup
+    .string()
+    .min(5)
+    .matches(passwordRules, { message: "Please enter stronger password" })
+    .required("Required"),
+  confirm_password: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Password must match")
+    .required("Required"),
+  dob: yup
+    .date("Please enter valid date")
+    .typeError("Please enter valid date")
+    .required("Required"),
+  phone: yup
+    .number()
+    .positive()
+    .integer()
+    .min(80000000, "Mobile number must be start with 8 or 9")
+    .max(100000000, "Mobile number must be start with 8 or 9")
+    .required("Please enter valid phone number"),
+  address: yup.string().required("Required"),
+  postcode: yup
+    .number()
+    .positive()
+    .integer()
+    .min(100000, "Postal code must be exactly 6 digits")
+    .max(1000000, "Postal code must be at exactly 6 digits")
+    .required("Please enter valid postal code"),
+  placeId: yup
+    .number()
+    .positive()
+    .integer()
+    .required("Please enter valid clinic"),
 });
